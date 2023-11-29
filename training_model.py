cb19cc0984bfac56ce2ab4fd62264a444cbf7797
Bhushan Jadhav <officialbhushan2020@gmail.com>
2023-11-29 19:29:53 +0530

    Initial setup for the training model. Python program added for splitting the dataset.

---
+++ training_model.py
@@ -0,0 +1,53 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix
+from tensorflow import keras
+from tensorflow.keras import layers
+
+# Load your dataset
+file_path = 'your_dataset.csv'  # Replace with your actual file path
+df = pd.read_csv(file_path)
+
+# Assuming your target variable is named 'is_laundered'
+X = df.drop('is_laundered', axis=1)  # Features
+y = df['is_laundered']  # Target variable
+
+# Split the dataset into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Standardize the features
+scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
+
+# Build the DNN model
+model = keras.Sequential([
+    layers.Dense(64, activation='relu', input_shape=(X_train.shape[1],)),
+    layers.Dense(32, activation='relu'),
+    layers.Dense(1, activation='sigmoid')
+])
+
+# Compile the model
+model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+
+# Train the model
+model.fit(X_train, y_train, epochs=10, batch_size=32, validation_split=0.2)
+
+# Predictions on the testing set
+y_pred_probs = model.predict(X_test)
+y_pred = (y_pred_probs > 0.5).astype("int32")
+
+# Calculate performance metrics
+accuracy = accuracy_score(y_test, y_pred)
+precision = precision_score(y_test, y_pred)
+recall = recall_score(y_test, y_pred)
+f1 = f1_score(y_test, y_pred)
+conf_matrix = confusion_matrix(y_test, y_pred)
+
+print(f"Accuracy: {accuracy}")
+print(f"Precision: {precision}")
+print(f"Recall: {recall}")
+print(f"F1 Score: {f1}")
+print("Confusion Matrix:")
+print(conf_matrix)
